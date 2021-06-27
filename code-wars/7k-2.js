ee112abadcaf8b63da78c1257e234a9b42d0393b
Karol Preiskorn <karol-preiskorn@users.noreply.github.com>
2021-06-27 14:19:51 +0200

    sychronizacja ex2 z github

--- code-wars/7k-2.js
+++ code-wars/7k-2.js
@@ -1,3 +1,10 @@
 String.prototype.toJadenCase = function() {
-    //...
+    var l = this.toLowerCase().split(" "); //?
+    l.map(this.toUpperCase() + this.substr(1).toLowerCase());
+    return this.split(" ").map(this.toUpperCase() + this.substr(1).toLowerCase());
 };
+
+var str = "How can mirrors be real if our eyes aren't real";
+//str.split(" ").charAt(0).toUpperCase(); //?
+//str.split(" "); //?
+str.toJadenCase();
