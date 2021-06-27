1a2e3155d4aec0a5396c0d5398d53b971b4baef5
Karol Preiskorn <karol-preiskorn@users.noreply.github.com>
2021-06-27 16:11:51 +0200

    ex2

--- code-wars/7k-2.js
+++ code-wars/7k-2.js
@@ -1,10 +1,26 @@
+//
+// https: //www.codewars.com/kata/5390bac347d09b7da40006f6/train/javascript
+//
 String.prototype.toJadenCase = function() {
-    var l = this.toLowerCase().split(" "); //?
-    l.map(this.toUpperCase() + this.substr(1).toLowerCase());
-    return this.split(" ").map(this.toUpperCase() + this.substr(1).toLowerCase());
+    var l = this.substr(0, 1).toUpperCase() + this.substr(1);
+    l = l.split(" ");
+    //?
+    const reducer = (accumulator, currentValue) =>
+        accumulator +
+        " " +
+        currentValue.substr(0, 1).toUpperCase() +
+        currentValue.substr(1); //?
+    return l.reduce(reducer);
 };
 
-var str = "How can mirrors be real if our eyes aren't real";
-//str.split(" ").charAt(0).toUpperCase(); //?
-//str.split(" "); //?
-str.toJadenCase();
+String.prototype.toJadenCase2 = function() {
+    return this.split(" ")
+        .map(function(word) {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        .join(" ");
+};
+
+module.exports.toJadenCase = String.prototype.toJadenCase;
+
+"how can mirrors be real if our eyes aren't real".toJadenCase2(); //?
--- spec/code-wars/7k-2Spec.js
+++ spec/code-wars/7k-2Spec.js
@@ -1,7 +1,8 @@
 describe("Tests", () => {
     it("test", () => {
+        var misc = require("../../code-wars/7k-2.js");
         var str = "How can mirrors be real if our eyes aren't real";
-        Test.assertEquals(
+        str.assertEquals(
             str.toJadenCase(),
             "How Can Mirrors Be Real If Our Eyes Aren't Real"
         );
